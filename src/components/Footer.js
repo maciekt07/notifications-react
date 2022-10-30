3726a5e091d8ea00c0a3c1c8abc255789f861088
maciekkoks <85953204+maciekkoks@users.noreply.github.com>
2022-10-30 20:23:14 +0100

    added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import "../styles/style.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      Made with 💙 By&nbsp;
+      <a target="_blank" rel="noreferrer" href="https://github.com/maciekt07">
+        maciekt07
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
