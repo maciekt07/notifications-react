cf046de403b12fa8d9e76bdbfdbc72f4d2b21689
maciekkoks <85953204+maciekkoks@users.noreply.github.com>
2022-10-30 16:54:08 +0100

    added scss

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { useState } from "react";
+import "./styles/style.css";
+import Push from "push.js";
+const App = () => {
+  const [Header, setHeader] = useState("");
+  const [Text, setText] = useState("");
+  const buttonClick = () => {
+    Push.create(Header, {
+      body: Text,
+    });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <input
+        value={Header}
+        onChange={(e) => setHeader(e.target.value)}
+        placeholder="Header"
+        type="text"
+      ></input>
+      <textarea
+        value={Text}
+        onChange={(e) => setText(e.target.value)}
+        placeholder="Text"
+      ></textarea>
+      <button onClick={buttonClick}>Create</button>
     </div>
   );
-}
+};
 
 export default App;
