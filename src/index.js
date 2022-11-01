a58f9d2a55bbc2a95172b62d5bf0792aada3c015
maciekkoks <85953204+maciekkoks@users.noreply.github.com>
2022-11-01 19:00:14 +0100

    basic changes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import X from "./X";
+
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <X />
+    <App />
   </React.StrictMode>
 );
 
