411a346cd010ab0601080e2671cc54368c7bae9f
maciekkoks <85953204+maciekkoks@users.noreply.github.com>
2023-01-01 18:29:09 +0100

    basic changes

---
+++ src/styles/index.js
@@ -0,0 +1,7 @@
+export { ButtonComponent } from "./Button.styled";
+export { FooterComponent, FooterEmoji } from "./Footer.styled";
+export { GlobalStyle } from "./Global.styled";
+export { HeaderInput, TextInput } from "./Inputs.styled";
+export { LastUseLabel } from "./LastUseLabel.styled";
+export { LinkComponent } from "./Link.styled";
+export { themeColors, btn } from "./Variables.styled";
